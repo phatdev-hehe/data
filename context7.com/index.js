1dd88be3be45fc9544b2caf051adce40f58334ac
phatdev-hehe <phatdev.hehe@gmail.com>
2025-05-04 15:23:34 +0700

    9

--- context7.com/index.js
+++ context7.com/index.js
@@ -1,37 +1,22 @@
-// https://github.com/upstash/context7/blob/master/src/lib/api.ts
-// https://github.com/upstash/context7/blob/master/src/lib/types.ts
-
-import { delay } from "es-toolkit";
 import { sort } from "fast-sort";
 import { formatNumber } from "intl-number-helper";
 import json2md from "json2md";
 import fs from "node:fs";
+import { $ } from "../index.js";
 
 const context7ApiBaseUrl = "https://context7.com/api";
 const dataPath = "data";
 
-const fetchData = async (
-  input,
-  { responseType = "json", delayMs = 3000, headers } = {}
-) => {
-  await delay(delayMs);
-
-  return await (
-    await fetch(`${context7ApiBaseUrl}/${input}`, { headers })
-  )[responseType]();
-};
-
-const libraries = sort(await fetchData("libraries")).asc((library) => [
-  library.settings.title,
-  library.settings.project,
-]);
+const libraries = sort(await $.fetch(`${context7ApiBaseUrl}/libraries`)).asc(
+  (library) => [library.settings.title, library.settings.project]
+);
 
 fs.writeFileSync(
   "readme.md",
   json2md({
-    img: { source: "favicon.ico" },
+    img: { source: "assets/favicon.ico" },
     table: {
-      headers: ["", "NAME", "REPO", "TOKENS", "SNIPPETS", "UPDATE", "STATE"],
+      headers: ["", "NAME", "REPO", "TOKENS", "SNIPPETS", "UPDATE", ""],
       rows: libraries.map((library, index) => {
         index += 1;
 
@@ -44,9 +29,9 @@ fs.writeFileSync(
           library.version.lastUpdate ?? "",
           `<img src=${
             {
-              finalized: "icons/completed-icon.svg",
-              initial: "icons/processing-icon.svg",
-              error: "icons/error-icon.svg",
+              finalized: "assets/completed-icon.svg",
+              initial: "assets/processing-icon.svg",
+              error: "assets/error-icon.svg",
               get parsed() {
                 return this.initial;
               },
@@ -69,20 +54,17 @@ fs.rmSync(dataPath, {
 fs.mkdirSync(dataPath);
 
 for (const [index, library] of libraries.entries()) {
-  const pathName = `/v1${library.settings.project}`;
-  const url = new URL(`${context7ApiBaseUrl}${pathName}`);
+  const url = new URL(`${context7ApiBaseUrl}/v1${library.settings.project}`);
 
   url.searchParams.set("tokens", library.version.totalTokens);
   url.searchParams.set("type", "txt");
 
   fs.writeFileSync(
     `${dataPath}/${index + 1}.txt`,
-    await fetchData(`${pathName}${url.search}`, {
+    await $.fetch(url, {
       responseType: "text",
       delayMs: 20000,
-      headers: {
-        "X-Context7-Source": "mcp-server",
-      },
+      headers: { "X-Context7-Source": "mcp-server" },
     })
   );
 }
